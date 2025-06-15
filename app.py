8ffed4be7d56b133ef7032a7d03f4225f9d8350f
anshita1510 <anshitabharwal@gmail.com>
2025-06-15 12:47:27 +0530

    app.py

--- app.py
+++ app.py
@@ -1,32 +1,4 @@
-# import streamlit as st
-# import pandas as pd
-# import seaborn as sns
-# import matplotlib.pyplot as plt
 
-# st.title("medical insurance data analysis Dashboard")
-
-# uploaded_file = st.file_uploader("C:/Users/Acer/Downloads/Medical_charges-main/insurance.csv", type=["csv"])
-
-# if uploaded_file:
-#     df = pd.read_csv(uploaded_file)
-#     st.subheader("Data Preview")
-#     st.dataframe(df)
-
-#     st.subheader("Data Preview (5 rows)")
-#     st.dataframe(df.head())
-#     st.subheader("Basic Statistics")
-#     st.write(df.describe())
-
-#     st.subheader("Missing Values")
-#     st.write(df.isnull().sum())
-
-#     st.subheader("Correlation Heatmap")
-#     fig, ax = plt.subplots(figsize=(10, 6))
-
-#     numeric_df = df.select_dtypes(include=['number'])
-#     sns.heatmap(numeric_df.corr(), annot=True, cmap="viridis", ax=ax)
-#     st.pyplot(fig)
-# app.py
 import streamlit as st
 import pandas as pd
 import plotly.express as px
